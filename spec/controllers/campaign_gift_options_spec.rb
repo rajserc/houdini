adc938d29f56a490b47e9ec1e6eba5f1d108a319
Eric Schultz <eric@wwahammy.com>
2019-01-16 13:05:21 -0600

    WIP

--- spec/controllers/campaign_gift_options_spec.rb
+++ spec/controllers/campaign_gift_options_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 require 'controllers/support/shared_user_context'
 
-describe CampaignGiftOptionsController, :type => :request do
+describe CampaignGiftOptionsController, :type => :controller do
   describe 'authorization' do
     include_context :shared_user_context
     describe 'reject unauthorized' do
@@ -11,14 +11,14 @@ describe CampaignGiftOptionsController, :type => :request do
       end
 
       describe 'update' do
-        include_context :open_to_campaign_editor, :put, :update, nonprofit_id: :__our_np, campaign_id: :__our_campaign
+        include_context :open_to_campaign_editor, :put, :update, nonprofit_id: :__our_np, campaign_id: :__our_campaign, id: "1"
       end
       describe 'destroy' do
-        include_context :open_to_campaign_editor, :delete, :destroy, nonprofit_id: :__our_np, campaign_id: :__our_campaign
+        include_context :open_to_campaign_editor, :delete, :destroy, nonprofit_id: :__our_np, campaign_id: :__our_campaign, id: "1"
       end
 
       describe 'update_order' do
-        include_context :open_to_campaign_editor, :put, :update_order, nonprofit_id: :__our_np, campaign_id: :__our_campaign
+        include_context :open_to_campaign_editor, :put, :update_order, nonprofit_id: :__our_np, campaign_id: :__our_campaign, id: "1"
       end
     end
 
@@ -28,7 +28,7 @@ describe CampaignGiftOptionsController, :type => :request do
       end
 
       describe 'show' do
-        include_context :open_to_all, :get, :show, nonprofit_id: :__our_np, campaign_id: :__our_campaign
+        include_context :open_to_all, :get, :show, nonprofit_id: :__our_np, campaign_id: :__our_campaign, id: "1"
       end
     end
   end
